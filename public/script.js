6e65d2303e459541ffe82829443d1d82f14311ff
JazlynnDenis-Mack <jazlynn.denis-mack25@compscihigh.org>
2025-04-08 20:29:53 +0000

    i cant believe this is wrong

--- public/script.js
+++ public/script.js
@@ -1,7 +1,23 @@
 // Create a function that tests the post route handler [2 pts]
-
-
-
-// Call the function [1 pt]
-// Show us the successful POST fetch [1 pt]
-
+base ="https://scaling-spoon-wrgv965qvr99c97r6-3000.app.github.dev/"
+async function sendData() {
+    const dataToSend = {
+        label: "Whisper",
+        effect: "Cannot talk above a whisper",
+        ingredients: "frog eyes, flew wings, elephant poop",
+        color: "Silver",
+        isExplosive: true
+    };
+  
+    const response = await fetch(base+ "/create-potion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dataToSend)
+    });
+  
+    const result = await response.json();
+    console.log(result);
+  }
+  sendData()
