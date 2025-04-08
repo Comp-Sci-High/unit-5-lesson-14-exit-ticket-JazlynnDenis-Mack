b22e9b7423237b1787ffc78a0bf22d7c0a5a626a
JazlynnDenis-Mack <jazlynn.denis-mack25@compscihigh.org>
2025-04-08 20:41:45 +0000

    done

--- public/script.js
+++ public/script.js
@@ -1,23 +1,34 @@
 // Create a function that tests the post route handler [2 pts]
-base ="https://scaling-spoon-wrgv965qvr99c97r6-3000.app.github.dev/"
-async function sendData() {
-    const dataToSend = {
-        label: "Whisper",
-        effect: "Cannot talk above a whisper",
-        ingredients: "frog eyes, flew wings, elephant poop",
-        color: "Silver",
-        isExplosive: true
-    };
-  
-    const response = await fetch(base+ "/create-potion", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(dataToSend)
-    });
-  
-    const result = await response.json();
-    console.log(result);
+let base ="https://scaling-spoon-wrgv965qvr99c97r6-3000.app.github.dev/"
+async function postCreature() {
+  try {
+      // fill in the path
+      const response = await fetch(base + "/create-potion", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            label: "whisper",
+    effect: "whisper",
+    ingredients: "tongue",
+    color: "silver",
+    isExplosive: true
+          })
+
+      });
+      const json = await response.json();
+      console.log(json);
+  } catch (error) {
+      console.error(error.message);
   }
-  sendData()
+}
+  
+  async function getPostion(name){
+    // fill in the path
+    const response = await fetch(base + "/create-postion/" + name)
+    const data = await response.json()
+    console.log(data)
+}
+
+getPostion("Whisper potion")
